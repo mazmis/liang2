8b20273d40c9edb84e928690157d65d7ed2faa15
M. Azmi Sudrajat <44989386+mazmis@users.noreply.github.com>
2022-06-06 15:12:23 +0700

    first commit

---
+++ src/com/bab07/ls01_.java
@@ -0,0 +1,21 @@
+// mempelajari soal array
+
+package com.bab07;
+
+public class ls01_ {
+
+	
+
+	public static void main(String[] args) {
+		int[] intArray = new int[3];
+		
+		intArray[0]= 2;
+		intArray[1] = 3;
+		intArray[2] = 8;
+		
+		System.out.println(intArray);
+		System.out.println(intArray[2]);
+		
+	}
+
+}
